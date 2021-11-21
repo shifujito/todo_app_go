1f8d4db630aa89692b9a01596444af6c9b2a75a0
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2021-11-21 17:49:43 +0900

    conncet psql db

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module todo
 
 go 1.17
+
+require (
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/lib/pq v1.10.4 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,58 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"text/template"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
 )
 
+type Todo struct {
+	Id   int
+	Task string
+}
+
+type Secret struct {
+	User   string `json: "User"`
+	Pass   string `json: "Pass"`
+	DbName string `json: "DbName"`
+}
+
+var Db *sql.DB
+var todo Todo
+
 var tmpl = template.Must(template.ParseGlob("template/*"))
+
+func ReadJson() (secret Secret) {
+	jsonFile, err := os.Open("secret.json")
+	if err != nil {
+		panic(err)
+	}
+	defer jsonFile.Close()
+	jsonData, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		panic(err)
+	}
+	json.Unmarshal(jsonData, &secret)
+	return
+}
+
+func init() {
+	secret := ReadJson()
+	Db, err := gorm.Open("postgres", "user="+secret.User+" dbname="+secret.DbName+" password="+secret.Pass+" sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	Db.AutoMigrate(&Todo{})
+	result := Db.Find(&todo)
+	fmt.Println(result)
+}
 
 func main() {
 	server := http.Server{
